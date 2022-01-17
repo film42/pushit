e9e44b7afbba861ecf978a85461acb63ac4b4a40
Garrett Thornburg <film42@gmail.com>
2022-01-16 19:26:50 -0700

    I pushed it!
    It works end to end but the code is very weak

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,128 @@ use openapi::apis::{configuration::Configuration, default_api as twilio_api};
 use std::env;
 use tide::prelude::*;
 use tide::{Body, Request, Response};
+use tokio::sync::mpsc::{error::TryRecvError, Receiver as MpscReceiver, Sender as MpscSender};
 
-async fn get_push_it(mut _req: Request<()>) -> tide::Result {
+struct TwilioCaller {
+    configuration: Configuration,
+    account_sid: String,
+    twilio_number: String,
+    to_number: String,
+    call_callback_url: String,
+}
+
+impl TwilioCaller {
+    async fn create_call(&self) -> Result<(), Box<dyn std::error::Error>> {
+        // Woof look at this boilerplate code.
+        let call = twilio_api::create_call(
+            &self.configuration,
+            &self.account_sid,
+            &self.twilio_number,
+            &self.to_number,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            Some(&self.call_callback_url),
+        )
+        .await?;
+
+        println!("Call made to twilio: {:?}", call);
+
+        Ok(())
+    }
+}
+
+async fn get_push_it(mut _req: Request<WebContext>) -> tide::Result {
     Ok(Response::builder(200)
         .body(Body::from_file("static/saltandpepper-pushit.mp3").await?)
         .build())
 }
 
-async fn post_twilio_call_callback(mut _req: Request<()>) -> tide::Result {
-    println!("Twilio call callback received!");
-
-    let payload = r#"
-<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-    <Play><a href="http://ocrmirror.org/files/music/remixes/Street_Fighter_2_Guile%27s_Theme_Goes_with_Metal_OC_ReMix.mp3">http://ocrmirror.org/files/music/remixes/Street_Fighter_2_Guile's_Theme_Goes_with_Metal_OC_ReMix.mp3</a></Play>
-</Response>"#.to_string();
+async fn post_twilio_call_callback(req: Request<WebContext>) -> tide::Result {
+    let payload = req.state().payload_for_caller();
+    println!(
+        "Twilio call callback POST request received! Responding with: {}",
+        &payload
+    );
 
     Ok(Response::builder(200)
         .body(payload)
-        .header("content-type", "text/xml")
+        .header("Content-Type", "text/xml")
         .build())
+}
+
+#[derive(Clone)]
+struct WebContext {
+    base_url: String,
+}
+
+impl WebContext {
+    fn payload_for_caller(&self) -> String {
+        let song_url = format!("{}/static/push-it.mp3", &self.base_url);
+        format!(
+            r#"<?xml version="1.0" encoding="UTF-8"?><Response><Play>{}</Play></Response>"#,
+            &song_url,
+        )
+        .to_string()
+    }
+}
+
+async fn debounce_events_and_make_twilio_call(
+    twilio: TwilioCaller,
+    mut receiver: MpscReceiver<usize>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        // The receiver was closed.
+        if receiver.recv().await.is_none() {
+            return Ok(());
+        }
+        println!("Received event. Debouncing...");
+
+        // Sleep for 10 seconds to debounce.
+        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+        // Throw away all the other events since we're about to make
+        // a call.
+        while let Ok(_) = receiver.try_recv() { /* ignore extra events */ }
+
+        // Push it!
+        println!("Triggering a call to twilio.");
+        twilio.create_call().await?;
+    }
 }
 
 async fn watch_for_kubernetes_deployment_changes(
     client: Client,
+    sender: MpscSender<usize>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let deployment_api: Api<Deployment> = Api::all(client);
     let params = ListParams::default();
@@ -49,9 +147,9 @@ async fn watch_for_kubernetes_deployment_changes(
         match event {
             WatchEvent::Added(_) | WatchEvent::Modified(_) => {
                 // Push it!
-                // NOTE: This will send several notifications at once. We should probably debounce
-                // per application?
-                println!("Something happened!!!");
+                println!("The kube api notified of a deployment event!!!");
+                sender.send(1337).await?;
+                println!("Watcher informed the twilio caller.");
             }
             _ => { /* ignore */ }
         }
@@ -62,7 +160,18 @@ async fn watch_for_kubernetes_deployment_changes(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut app = tide::new();
+    // Configs from env
+    let account_sid = env::var("TWILIO_ACCOUNT_SID").expect("Failed to parse Account SID");
+    let api_key = env::var("TWILIO_API_KEY").expect("Failed to parse API Key");
+    let api_key_secret = env::var("TWILIO_API_KEY_SECRET").expect("Failed to parse API Key Secret");
+    let twilio_number = env::var("TWILIO_PHONE_NUMBER").expect("Failed to parse 'from' number");
+    let to_number = env::var("TO_NUMBER").expect("Failed to parse 'to' number");
+    let base_url = env::var("APPLICATION_BASE_URL").expect("Failed to parse 'base url' value");
+
+    // Configure the webserver that handles callbacks and static assets.
+    let mut app = tide::with_state(WebContext {
+        base_url: base_url.clone(),
+    });
     app.at("/twilio/call/callback")
         .post(post_twilio_call_callback);
     app.at("/static/push-it.mp3").get(get_push_it);
@@ -77,58 +186,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .expect("could not accept connections");
     });
 
-    let account_sid = env::var("TWILIO_ACCOUNT_SID").expect("Failed to parse Account SID");
-    let api_key = env::var("TWILIO_API_KEY").expect("Failed to parse API Key");
-    let api_key_secret = env::var("TWILIO_API_KEY_SECRET").expect("Failed to parse API Key Secret");
-    let from = env::var("TWILIO_PHONE_NUMBER").expect("Failed to parse 'from' number");
-    let to = env::var("TO_NUMBER").expect("Failed to parse 'to' number");
-
+    // Configure Twilio
+    let call_callback_url = format!("{}/twilio/call/callback", &base_url).to_string();
+    println!("Twilio callback: {}", call_callback_url);
     let mut twilio_config = Configuration::default();
     twilio_config.basic_auth = Some((api_key, Some(api_key_secret)));
+    let twilio_caller = TwilioCaller {
+        configuration: twilio_config,
+        account_sid,
+        twilio_number,
+        to_number,
+        call_callback_url,
+    };
 
-    //    // Asynchronously send the message "Ahoy, Rustacean! 🦀" to the `to` number from your Twilio phone number.
-    //    let message = twilio_api::create_message(
-    //        &twilio_config,
-    //        &account_sid,
-    //        &to,
-    //        None,
-    //        None,
-    //        None,
-    //        Some("Ahoy, Rustacean! 🦀"),
-    //        None,
-    //        None,
-    //        Some(&from),
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //        None,
-    //    )
-    //    .await;
-    //
-    //    let result = match message {
-    //        Ok(result) => result,
-    //        Err(error) => panic!("Something went wrong, {:?}", error),
-    //    };
+    let (kube_events_sender, kube_events_receiver) = tokio::sync::mpsc::channel(100);
+    tokio::spawn(async move {
+        debounce_events_and_make_twilio_call(twilio_caller, kube_events_receiver)
+            .await
+            .unwrap();
+    });
 
     // Initialize the kube controller
     // Infer the runtime environment and try to create a Kubernetes Client
     let client = Client::try_default().await?;
-    watch_for_kubernetes_deployment_changes(client)
+    watch_for_kubernetes_deployment_changes(client, kube_events_sender)
         .await
         .expect("failed to watch");
 
-    //    loop {
-    //        tokio::time::sleep(std::time::Duration::from_secs(1));
-    //    }
-
-    println!("Hello, world!");
+    println!("Exiting...");
 
     Ok(())
 }
